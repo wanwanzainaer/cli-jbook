246363788819fe3e4a10c838493b9cddd0e157a6
simon <wanwanzainaer@gmail.com>
2021-02-23 23:06:46 -0800

    Add cells router POST GET method

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -1,11 +1,26 @@
 import path from 'path';
 import { Command } from 'commander';
 import { serve } from 'local-api';
+
+const isProduction = process.env.NODE_ENV === 'production';
+
 export const serveCommand = new Command()
   .command('serve [filename')
   .description('Open a file for editing')
   .option('-p, --port <number>', 'port to run server on', '4005')
-  .action((filename = 'notebook.js', options: { port: string }) => {
-    const dir = path.join(process.cwd(), path.dirname(filename));
-    serve(parseInt(options.port), filename, dir);
+  .action(async (filename = 'notebook.js', options: { port: string }) => {
+    try {
+      const dir = path.join(process.cwd(), path.dirname(filename));
+      await serve(parseInt(options.port), filename, dir, !isProduction);
+      console.log(
+        `Opened ${filename}. Naviage to http://localhost:${options.port} to edit the file`
+      );
+    } catch (err) {
+      if (err.code === 'EADDRINUSE') {
+        console.error('Port is in use. Try running on a different port');
+      } else {
+        console.log('Here is the problem ', err.message);
+      }
+      process.exit(1);
+    }
   });
---
+++ packages/local-api/src/routes/cells.ts
@@ -0,0 +1,50 @@
+import express from 'express';
+import fs from 'fs/promises';
+import path from 'path';
+
+interface Cell {
+  id: string;
+  content: string;
+  type: 'text' | 'code';
+}
+
+export const createCellsRouter = (filename: string, dir: string) => {
+  const router = express.Router();
+
+  router.use(express.json());
+
+  const fullPath = path.join(dir, filename);
+
+  router.get('/cells', async (req, res) => {
+    try {
+      const result = await fs.readFile(fullPath, { encoding: 'utf-8' });
+
+      res.send(JSON.parse(result));
+    } catch (err) {
+      if (err.code === 'ENOENT') {
+        await fs.writeFile(fullPath, '[]', 'utf-8');
+        res.send([]);
+      } else {
+        throw err;
+      }
+    }
+    // Make sure the cell storage file exists
+    // If it does not exist, add in a default list of cells
+    // Read the file
+    // Parse a list of cells out of it
+    // Send list of cells back to browser
+  });
+
+  router.post('/cells', async (req, res) => {
+    // Take the list of cells from the request obj
+    // serialize them
+    const { cells }: { cells: Cell[] } = req.body;
+
+    // Write the cells into the file
+    await fs.writeFile(fullPath, JSON.stringify(cells), 'utf-8');
+
+    res.send({ status: 'ok' });
+  });
+
+  return router;
+};
