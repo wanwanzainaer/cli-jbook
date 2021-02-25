ea39d0b1e6be6d4ec8beb2b0f527280d47367be4
simon <wanwanzainaer@gmail.com>
2021-02-25 00:01:17 -0800

    Add debouncing

--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -10,6 +10,8 @@ export const serve = (
 ) => {
   const app = express();
 
+  app.use(createCellsRouter(filename, dir));
+
   if (useProxy) {
     app.use(
       createProxyMiddleware({
@@ -23,7 +25,6 @@ export const serve = (
     app.use(express.static(path.dirname(packagePath)));
   }
 
-  app.use(createCellsRouter(filename, dir));
   return new Promise<void>((resolve, reject) => {
     app.listen(port, resolve).on('error', reject);
   });
